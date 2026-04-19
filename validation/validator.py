9aed891d8a826cb16e0fac482b7f487b0a6b5203
Roman_Havrylov <roman14082004@gmail.com>
2026-04-19 23:25:37 +0200

    add validation

---
+++ validation/validator.py
@@ -0,0 +1,8 @@
+REQUIRED_FIELDS = ["id", "type"]
+
+def validate_event(event: dict) -> bool:
+    """Check that all required fields are present and non-empty."""
+    for field in REQUIRED_FIELDS:
+        if field not in event or not event[field]:
+            return False
+    return True
